cbb2a38c0e6ae472f2beb2fe723bdc516f7cfe71
Alberto Wollmann Dietrich <alberto.dietrich@saqueepague.com.br>
2023-07-22 19:31:49 -0300

    Adding github actions

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -2,12 +2,15 @@
 # Load project version
 PKG_VERSION=$(cat ./package.json | jq -r ".version")
 # First, clean the project and prevous releases
+echo "Removing old files..."
 rm -rf ./dist/ ./node_modules/ ./*.tar.gz
 # Compile it again
+echo "Compiling code..."
 npm ci && npm run build && mage -v
 # Make distribution files
+echo "Compacting release files..."
 cd ./dist/
-tar --exclude="gpx*arm*" -cvzf ../albertowd-oraclegrafana-datasource-${PKG_VERSION}-bundle.tar.gz ./
+tar --exclude="gpx*arm*" -cvzf ../albertowd-oraclegrafana-datasource-bundle-${PKG_VERSION}.tar.gz ./
 tar --exclude="gpx*arm*" --exclude="gpx*linux*" --exclude="gpx*windows*" -cvzf ../albertowd-oraclegrafana-datasource-${PKG_VERSION}-darwin-amd64.tar.gz ./
 tar --exclude="gpx*arm*" --exclude="gpx*darwin*" --exclude="gpx*windows*" -cvzf ../albertowd-oraclegrafana-datasource-${PKG_VERSION}-linux-amd64.tar.gz ./
 tar --exclude="gpx*arm*" --exclude="gpx*darwin*" --exclude="gpx*linux*" -cvzf ../albertowd-oraclegrafana-datasource-${PKG_VERSION}-windows-amd64.tar.gz ./
